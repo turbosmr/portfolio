aeec26cc5941080dc539caf68032116b240d2ed7
STEVE RENTSCHLER <steverentschler@STEVEs-MacBook-Pro-2.local>
2019-09-02 17:54:15 -0700

    Updates

---
+++ precache-manifest.d69e5fcad5528b5a10dc07d4adebf9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1583f17fd07968f3058668c97508c6f8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a9143600ec6d9fcbc6b7",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "52966132cf442b03e799",
+    "url": "/portfolio/static/js/2.63322133.chunk.js"
+  },
+  {
+    "revision": "a9143600ec6d9fcbc6b7",
+    "url": "/portfolio/static/js/main.bfa420c1.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
