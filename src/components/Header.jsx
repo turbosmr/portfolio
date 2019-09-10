fd7865f076bdb9d4683baa0ca245b388470efabe
STEVE RENTSCHLER <steverentschler@sfs-wifi-dhcp-10-142-5-241.wifi.sfsu.edu>
2019-09-09 21:33:39 -0700

    fixed header img path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div className="Header text-center" style={{ marginBottom: 0 }}>
+            <div className="text-center" style={{ marginBottom: 0, backgroundImage: 'url(IMG_0787copy.jpeg)' }}>
                 <div className="HeaderText">
                     <p>Steve Rentschler</p>
                 </div>
