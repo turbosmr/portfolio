11a43e24e2155ddd6651acb1a944d00df28a833a
STEVE RENTSCHLER <steverentschler@STEVEs-MacBook-Pro-2.local>
2019-09-10 18:22:40 -0700

    testing
    testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,9 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div className="Header text-center" style={{ marginBottom: 0, backgroundImage: 'url(images/IMG_0787copy.jpeg)' }}>
-                <div className="HeaderText">
-                    <p>Steve Rentschler</p>
-                </div>
-                <div className="SubheaderText">
-                    <p>Software Engineer</p>
-                </div>
+            <div className="Header text-center" style={{ marginBottom: 0, backgroundImage: 'url(images/IMG_0787copy.jpeg)' }}>      
+                    <p className= "title">Steve Rentschler</p>
+                    <p className= "subtitle">Software Engineer</p>
             </div>
         )
     }
