1d2d93765ea9ad608a7f042c0b6d04d526828cea
STEVE RENTSCHLER <steverentschler@STEVEs-MacBook-Pro-2.local>
2019-11-10 17:06:35 -0800

    Updates

--- precache-manifest.954908821b38da96677b7e9317372a3c.js
+++ precache-manifest.954908821b38da96677b7e9317372a3c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7bb7b93271a3e0b896039f1663d65f4b",
+    "revision": "7707f155fbd4a9a6edeeafdc13ea293c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "945dc359e5f7054ac774",
+    "revision": "99888312067fb0d3b9eb",
     "url": "/portfolio/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.88a348ac.chunk.js"
   },
   {
-    "revision": "945dc359e5f7054ac774",
-    "url": "/portfolio/static/js/main.1a105eeb.chunk.js"
+    "revision": "99888312067fb0d3b9eb",
+    "url": "/portfolio/static/js/main.3112b567.chunk.js"
   },
   {
     "revision": "4bc79a2d54f74fff90c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6db052758300e7e0a69e83992ed838ef.js"
+  "/portfolio/precache-manifest.954908821b38da96677b7e9317372a3c.js"
 );
 
 self.addEventListener('message', (event) => {
