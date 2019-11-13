b284b7dce9eed59384e062e5ae3a28f8e20c120e
STEVE RENTSCHLER <steverentschler@sfs-wifi-dhcp-10-142-14-180.wifi.sfsu.edu>
2019-11-12 16:24:27 -0800

    Updates

--- precache-manifest.049cb3f96928ba99ff6e7991ed7851c4.js
+++ precache-manifest.049cb3f96928ba99ff6e7991ed7851c4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6343fff93cf9baf72b10a51578a3313e",
+    "revision": "7ba83362dbf7b42e45ab22ae002e0a27",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "606e6f1818252183fd6c",
+    "revision": "b64e61b08b041064c7dc",
     "url": "/portfolio/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.88a348ac.chunk.js"
   },
   {
-    "revision": "606e6f1818252183fd6c",
-    "url": "/portfolio/static/js/main.71edbbf1.chunk.js"
+    "revision": "b64e61b08b041064c7dc",
+    "url": "/portfolio/static/js/main.e0629093.chunk.js"
   },
   {
     "revision": "4bc79a2d54f74fff90c2",
